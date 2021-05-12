0a91bd9b06313033150558b923fd0ff8c9ae9459
bringmetheaugust <bringmetheaugust@gmail.com>
2021-05-12 19:28:35 +0300

    add bot mailing

---
+++ TELEGRAM_BOT.md
@@ -0,0 +1,14 @@
+# Garage🏚 bot
+
+## Description
+
+*Telegram* bot docs for *@Garage* project
+
+## Join
+
+Add bot to *Telegram* group by name `@fedorenka_garage_bot`.    
+Bot send message only for special groups. Groups are specified in the project configuration.
+
+## Docs
+
+[Bot link](t.me/fedorenka_garage_bot).    
--- server/src/controllers/callme.js
+++ server/src/controllers/callme.js
@@ -1,10 +1,26 @@
-export default function({ body }, res) {
+import fetch from 'node-fetch';
+
+const { TELEGRAM_TOKEN, TELEGRAM_CHAT_ID } = process.env;
+
+export default async function({ body }, res) {
     try {
         const { name, phone } = body;
 
         if (!phone) throw Error;
 
-        res.sendStatus(200);
+        const message = encodeURI(`<b>Запрос на звонок!</b>\n👨‍💼: ${ name || 'не указано' }\n☎️: ${phone}`);
+
+        try {
+            const res = await fetch(
+                `https://api.telegram.org/bot${ TELEGRAM_TOKEN || '1886946121:AAEehYw6n3xz1eSCljt3gkZzc3lb0Z3epbA'}/sendMessage?chat_id=${ TELEGRAM_CHAT_ID || -279746682 }&parse_mode=html&text=${message}`
+            );
+
+            if (!res.ok) throw Error;
+
+            res.sendStatus(200);
+        } catch(err) {
+            res.sendStatus(500);
+        }
     } catch(err) {
         res.sendStatus(400);
     }
