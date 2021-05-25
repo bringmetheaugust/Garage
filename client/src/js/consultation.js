0c79ad5f7b45841e62d2a0ea7ef17017d4bac643
bringmetheaugust <bringmetheaugust@gmail.com>
2021-05-26 00:52:37 +0300

    fix consultation controller

--- client/src/js/consultation.js
+++ client/src/js/consultation.js
@@ -2,6 +2,7 @@ import Notification from './notification.js';
 
 document.getElementById('consultation').addEventListener('submit', async e => {
     e.preventDefault();
+    e.target.classList.add('loading');
     
     try {
         const res = await fetch('http://localhost:2101/api/callme', {
@@ -22,5 +23,7 @@ document.getElementById('consultation').addEventListener('submit', async e => {
             case 403: return new Notification('error', 'Вы уже отправляли запрос<br>Попробуйте еще раз через 10 минут');
             default: new Notification('error', 'Извините, произошла ошибка');
         }
+    } finally {
+        e.target.classList.remove('loading');
     }
 });
