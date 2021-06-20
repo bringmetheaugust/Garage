5be047ea9425069db61869cd82c11d87e072fc63
bringmetheaugust <bringmetheaugust@gmail.com>
2021-06-20 22:59:01 +0300

    rebuild Webpack config

--- client/config/webpack/webpack.dev.js
+++ client/config/webpack/webpack.dev.js
@@ -3,34 +3,34 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 const config = require('./webpack.config.js');
 
-config.output = {
-	path: path.resolve(__dirname, './dist'),
-	filename: 'bundle.js',
-	publicPath: '/'
-};
-
-config.devtool = 'source-map';
-
-config.devServer = {
-	contentBase: './src',
-	watchContentBase: true,
-	overlay: true,
-	historyApiFallback: {
-		rewrites: [
-			{ from: /./, to: '/404.html' },
-		],
+const devConfig = {
+	...config,
+	output: {
+		path: path.resolve(__dirname, './dist'),
+		filename: 'bundle.js',
+		publicPath: '/'
 	},
-	port: 2100,
-	hot: true,
-	inline: true,
-	clientLogLevel: 'none',
-	disableHostCheck: true
+	devtool: 'source-map',
+	devServer: {
+		contentBase: './src',
+		watchContentBase: true,
+		overlay: true,
+		historyApiFallback: {
+			rewrites: [
+				{ from: /./, to: '/404.html' },
+			],
+		},
+		port: 2100,
+		hot: true,
+		inline: true,
+		clientLogLevel: 'none',
+		disableHostCheck: true
+	},
+	plugins: [
+		...config.plugins,
+		new MiniCssExtractPlugin()
+	],
+	mode: 'development'
 };
 
-config.plugins.push(
-	new MiniCssExtractPlugin(),
-);
-
-config.mode = 'development';
-
-module.exports = config;
+module.exports = devConfig;
--- client/config/webpack/webpack.prod.js
+++ client/config/webpack/webpack.prod.js
@@ -3,18 +3,20 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 const config = require('./webpack.config.js');
 
-config.output = {
-	path: path.resolve(__dirname, '../../dist'),
-	filename: '[chunkhash].js',
-	publicPath: ''
+const prodConfig = {
+	...config,
+	output: {
+		path: path.resolve(__dirname, '../../dist'),
+		filename: '[chunkhash].js',
+		publicPath: ''
+	},
+	output: [
+		...config.plugins,
+		new MiniCssExtractPlugin({
+			filename: '[name].[hash].css',
+		})
+	],
+	mode: 'production'
 };
 
-config.plugins.push(
-	new MiniCssExtractPlugin({
-		filename: '[name].[hash].css',
-	}),
-);
-
-config.mode = 'production';
-
-module.exports = config;
+module.exports = prodConfig;
