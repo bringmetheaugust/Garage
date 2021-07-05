ec394745476aa0ce7a5354a8fce6259fe13dd594
bringmetheaugust <bringmetheaugust@gmail.com>
2021-07-05 21:20:55 +0300

    rebuild translates

--- client/config/webpack/webpack.config.js
+++ client/config/webpack/webpack.config.js
@@ -90,7 +90,13 @@ export default {
 				content: translates,
 				lang: translates.lang
 			})
-		))
+		)),
+		new HtmlWebpackPlugin({
+			filename: '404.html',
+			template: './src/pages/404.pug',
+			favicon: './src/media/favicon.png',
+			inject: 'body'
+		})
 		// ...mapPugPages('./src/pages/*') // ? for multi Pug pages
 	]
 };
--- client/src/index.js
+++ client/src/index.js
@@ -1,4 +1,5 @@
 import './styles/pages/index.sass';
+import './styles/pages/404.sass';
 import './styles/reset.css';
 
 import './controllers/common.js';
